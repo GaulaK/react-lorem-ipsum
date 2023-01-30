d02a3fdbbdf103ba6802a1052ee072d80a52be28
Gauthier Portefaix <portefaixgauthier@gmail.com>
2023-01-30 12:50:23 +0100

    include Description and Title components

---
+++ src/components/Description.js
@@ -0,0 +1,6 @@
+const Description = (props) => {
+  console.log("test");
+  return <p className={props.className}>{props.text}</p>;
+};
+
+export default Description;
---
+++ src/components/Title.js
@@ -0,0 +1,5 @@
+const Title = (props) => {
+  return <h1 className={props.className}>{props.name}</h1>;
+};
+
+export default Title;
